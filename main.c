1d419c1f0a2406bb58111afab08814a7f344d398
Prom1234pro <promiseimadonmwinyi@gmail.com>
2023-04-26 17:10:50 -0400

    created main.c and first task

--- main.c
+++ main.c
@@ -9,48 +9,56 @@
 
 int main(int argc, char **argv)
 {
-	char *line = NULL;
-	size_t bufsize = 0;
-	ssize_t characters_read;
-	pid_t pid;
-	int interactive = isatty(STDIN_FILENO);
-	int status;
+    char *line = NULL;
+    size_t bufsize = 0;
+    ssize_t characters_read;
+    pid_t pid;
+    int interactive = isatty(STDIN_FILENO);
+    int status;
 
-	while (1)
-	{
-		if (interactive && argc == 1)
-			printf("($) ");
-		characters_read = getline(&line, &bufsize, stdin);
-		if (characters_read == -1)
-			break;
-		if (line[characters_read - 1] == '\n')
-			line[characters_read - 1] = '\0';
-		if (strcmp(line, "exit") == 0)
-			break;
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			argv[0] = line;
-			argv[1] = NULL;
-			if (execve(argv[0], argv, NULL) == -1)
-				fprintf(stderr, "./%s: 1: %s: not found\n", SHELL_NAME, argv[0]);
-		}
-		else
-		{
-			if (wait(&status) == -1)
-			{
-				perror("wait");
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
+    while (1)
+    {
+        if (interactive && argc == 1)
+            printf("($) ");
+        characters_read = getline(&line, &bufsize, stdin);
+        if (characters_read == -1)
+            break;
+        if (line[characters_read - 1] == '\n')
+            line[characters_read - 1] = '\0';
+        if (strcmp(line, "exit") == 0)
+            break;
+        char *token;
+        char *args[10];
+        int i = 0;
+        token = strtok(line, " ");
+        while (token != NULL)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            if (execve(args[0], args, NULL) == -1)
+                fprintf(stderr, "./%s: 1: %s: not found\n", SHELL_NAME, args[0]);
+        }
+        else
+        {
+            if (wait(&status) == -1)
+            {
+                perror("wait");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 
-	if (line)
-		free(line);
-	exit(EXIT_SUCCESS);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
 }
