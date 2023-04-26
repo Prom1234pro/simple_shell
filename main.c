18627ad4072532d2e1596d941926a1b8cadb2a51
Prom1234pro <promiseimadonmwinyi@gmail.com>
2023-04-26 18:51:02 -0400

    created main.c and first task

--- main.c
+++ main.c
@@ -7,17 +7,17 @@
 #define BUFFER_SIZE 1024
 #define SHELL_NAME "hsh"
 
-int main(int argc, char **argv)
+int main()
 {
     char *line = NULL;
     size_t bufsize = 0;
     ssize_t characters_read;
     pid_t pid;
     int interactive = isatty(STDIN_FILENO);
-    int status;
+    int status = 0;
+    char *token;
     char *args[10];
     int i;
-	char *token;
 
     while (1)
     {
@@ -29,55 +29,58 @@ int main(int argc, char **argv)
         if (line[characters_read - 1] == '\n')
             line[characters_read - 1] = '\0';
         if (strcmp(line, "exit") == 0)
+            break;
+        else if (strncmp(line, "cd ", 3) == 0)
         {
-            if (argc > 1)
+            char *dir = line + 3;
+            if (*dir == '\0')
+                dir = getenv("HOME");
+            else if (strcmp(dir, "-") == 0)
             {
-                int exit_status = atoi(argv[1]);
-                exit(exit_status);
+                dir = getenv("OLDPWD");
+                printf("%s\n", dir);
             }
+            if (dir && chdir(dir) == -1)
+                perror(dir);
             else
             {
-                exit(EXIT_SUCCESS);
+                setenv("OLDPWD", getenv("PWD"), 1);
+                setenv("PWD", getcwd(NULL, 0), 1);
             }
-        }
-        token = line;
-        i = 0;
-        while (*token != '\0')
-        {
-            while (*token == ' ')
-                token++;
-            if (*token == '\0')
-                break;
-            args[i++] = token;
-            while (*token != ' ' && *token != '\0')
-                token++;
-            if (*token == '\0')
-                break;
-            *token++ = '\0';
-        }
-        args[i] = NULL;
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            if (execvp(args[0], args) == -1)
-                fprintf(stderr, "./%s: 1: %s: not found\n", SHELL_NAME, args[0]);
         }
         else
         {
-            if (wait(&status) == -1)
+            i = 0;
+            token = strtok(line, " ");
+            while (token != NULL)
             {
-                perror("wait");
+                args[i++] = token;
+                token = strtok(NULL, " ");
+            }
+            args[i] = NULL;
+            pid = fork();
+            if (pid == -1)
+            {
+                perror("fork");
                 exit(EXIT_FAILURE);
+            }
+            else if (pid == 0)
+            {
+                if (execvp(args[0], args) == -1)
+                    fprintf(stderr, "./%s: 1: %s: not found\n", SHELL_NAME, args[0]);
+            }
+            else
+            {
+                if (wait(&status) == -1)
+                {
+                    perror("wait");
+                    exit(EXIT_FAILURE);
+                }
             }
         }
     }
 
     if (line)
         free(line);
-    exit(EXIT_SUCCESS);
+    exit(status);
 }
