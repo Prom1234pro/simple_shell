e659206b5e2091c6caf7375f149e0a8750bab71a
root <root@kali.school.net>
2023-04-26 14:52:47 -0400

    created main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/**
+ * main - main function
+ * @argc: command line arguments
+ * @argv: command line arguments
+ *
+ * Return:int
+ */
+
+int main(int argc, char **argv)
+{
+	return (0);
+}
